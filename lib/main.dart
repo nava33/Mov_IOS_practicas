c1e5a37f08966bffe731894c776f7539852ff3b7
FlutLab.io <>
2021-05-09 15:22:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:nava/screens/note_list.dart';
+import 'package:nava/screens/note_detail.dart';
 
-/// ESTA APLICACION DE EJEMPLO CONSTRUIDA EN FLUTTER
-/// MUESTRA UN EJEMPLO DE CÓMO USAR LOS WIDGETS COLUMNA Y FILA.
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'NoteKeeper',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Combinado Nava')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Container(
-          color: Colors.greenAccent,
-          width: 1000,
-          height: 571,
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(16),
-                color: Colors.orange,
-                width: 1000,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(color: Colors.red, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.blue, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.green, width: 85, height: 100),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                padding: const EdgeInsets.all(16),
-                color: Colors.orange,
-                width: 1000,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(color: Colors.green, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.blue, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.red, width: 85, height: 100),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                padding: const EdgeInsets.all(16),
-                color: Colors.orange,
-                width: 1000,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(color: Colors.red, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.blue, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.green, width: 85, height: 100),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                padding: const EdgeInsets.all(16),
-                color: Colors.orange,
-                width: 1000,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(color: Colors.green, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.blue, width: 85, height: 100),
-                    SizedBox(width: 16),
-                    Container(color: Colors.red, width: 85, height: 100),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: NoteList(),
     );
   }
 }
